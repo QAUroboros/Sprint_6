d616e6d93b01655024c0bc2cfe64109d45b5882a
Artem Krivoshein <artem.krivoshein@tele2.kz>
2024-09-14 23:07:25 +0500

    обновил фикстуру и внешние связи

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,10 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 @pytest.fixture
 def open_browser():
     driver = webdriver.Firefox()
     driver.get("https://qa-scooter.praktikum-services.ru/")
-    driver.implicitly_wait(30)
     yield driver
     driver.quit()
