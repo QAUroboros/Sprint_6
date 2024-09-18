11937498e3465d7d0555cfc7989dd13b2737c52b
Artem Krivoshein <artem.krivoshein@tele2.kz>
2024-09-18 14:51:39 +0500

    убрал неявные ожидания используя метод wait_for_element

--- page_object/base_page.py
+++ page_object/base_page.py
@@ -15,34 +15,37 @@ class BasePage:
     @allure.step("Кликнуть на элемент: {locator}")
     def click_element(self, locator):
         self.scroll_to_element(locator)
-        element = WebDriverWait(self.driver, 30).until(expected_conditions.element_to_be_clickable(locator))
+        element = self.wait_for_element(locator)
         element.click()
 
     @allure.step("Ввести текст в элемент: {locator}")
     def send_keys(self, locator, text):
-        element = WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(locator))
+        element = self.wait_for_element(locator)
         element.clear()
         element.send_keys(text)
 
     @allure.step("Получить текст элемента: {locator}")
     def get_element_text(self, locator):
-        element = WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(locator))
+        element = self.wait_for_element(locator)
         return element.text
 
     @allure.step("Ожидание видимости элемента: {locator}")
     def wait_for_element(self, locator):
         return WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(locator))
 
+    @allure.step("Переключиться на новое окно браузера")
     def switch_to_new_window(self):
         self.driver.switch_to.window(self.driver.window_handles[-1])
 
+    @allure.step("Получить текущий URL")
     def get_current_url(self):
         return self.driver.current_url
 
+    @allure.step("Закрыть текущее окно браузера")
     def close_current_window(self):
         self.driver.close()
 
     @allure.step("Отправить специальную клавишу: {key} в элемент: {locator}")
     def send_special_key(self, locator, key):
-        element = WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(locator))
+        element = self.wait_for_element(locator)
         element.send_keys(key)
